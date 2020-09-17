c058b47e3dd0327ba76cc593a72ae5310068d953
Jin-Woo Kim <dnehd6739@naver.com>
2020-09-17 23:58:27 +0900

    SHA-256

--- BOJ java.ver/src/BOJ10930.java
+++ BOJ java.ver/src/BOJ10930.java
@@ -10,26 +10,23 @@ public class BOJ10930 {
 		// TODO Auto-generated method stub
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-		String base = in.readLine();
-
-		try{
-
-			MessageDigest digest = MessageDigest.getInstance("SHA-256");
-			byte[] hash = digest.digest(base.getBytes("UTF-8"));
-			StringBuffer hexString = new StringBuffer();
-
-			for (int i = 0; i < hash.length; i++) {
-				String hex = Integer.toHexString(0xff & hash[i]);
-				if(hex.length() == 1) hexString.append('0');
-				hexString.append(hex);
+		String sha = in.readLine();
+		try {
+			MessageDigest sh = MessageDigest.getInstance("SHA-256");
+			sh.update(sha.getBytes());
+			byte byteData[] = sh.digest();
+			StringBuilder sb = new StringBuilder();
+			for(int i=0;i<byteData.length;i++) {
+				sb.append(Integer.toString((byteData[i]&0xff)+0x100,16).substring(1));
 			}
-
-			//Ãâ·Â
-			System.out.println(hexString.toString());
-
-		} catch(Exception ex){
-			throw new RuntimeException(ex);
+			sha = sb.toString();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		
+		System.out.println(sha);
+		
 	}
 
 }
