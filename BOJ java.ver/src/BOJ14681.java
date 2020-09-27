b744a7687268362c0b402e1ee6cafa152802fefd
Jin-Woo Kim <dnehd6739@naver.com>
2020-09-28 02:38:58 +0900

    Problem

---
+++ BOJ java.ver/src/BOJ14681.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ14681 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int x = Integer.parseInt(br.readLine());
+		int y = Integer.parseInt(br.readLine());
+		
+		if(x>0 && y>0) {
+			System.out.print(1);
+		}else if(x<0 && y>0) {
+			System.out.print(2);
+		}else if(x<0 && y<0) {
+			System.out.print(3);
+		}else {
+			System.out.print(4);
+		}
+	}
+
+}
