e3c4b0cfd2d91616118b8e3de6b9361ace36de4f
Jin-Woo Kim <dnehd6739@naver.com>
2020-09-28 16:47:04 +0900

    Problem

--- BOJ java.ver/src/BOJ2108.java
+++ BOJ java.ver/src/BOJ2108.java
@@ -2,56 +2,49 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.Collections;
 
 public class BOJ2108 {
-	
-	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-	Scanner scan = new Scanner(System.in);
-	
-	
+			
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		BOJ2108 Main = new BOJ2108();
-		Main.start();
-
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int len = Integer.parseInt(br.readLine());
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		int[] arr = new int[8001];
+		int val = 0;
+		int sum = 0;
+		
+		for(int i=0; i<len; i++) {
+			val = Integer.parseInt(br.readLine());
+			arr[4000+val]++;
+			sum += val;
+			list.add(val);
+		}
+		
+		System.out.println(Math.round((double)sum/len));
+		
+		Collections.sort(list);
+		System.out.println(list.get(len/2));
+		
+		int[] two_num = new int[2];
+		int mode = 0;
+		for(int i=0; i<arr.length; i++) {
+			if(arr[i]>mode) {
+				mode = arr[i];
+				two_num[0]=i-4000; two_num[1]=0;
+			}else if(arr[i]==mode && mode!=0 && two_num[1]==0) {
+				two_num[1]=i-4000;
+			}
+		}
+		if(two_num[1]==0) {
+			System.out.println(two_num[0]);
+		}else {
+			System.out.println(two_num[1]);
+		}
+		
+		System.out.println(list.get(len-1)-list.get(0));
+		
 	}
-	public void start() throws IOException{
-		solution();
-	}
-	public void solution() throws IOException {
-			int N = Integer.parseInt(in.readLine());
-	        int[] arr = new int[N];
-	        int[] check = new int[8001];
-	        int sum = 0;
-	    
-	        for(int i=0 ; i<N ; i++){
-	            arr[i] =  Integer.parseInt(in.readLine());
-	            check[arr[i]+4000]++;
-	            sum += arr[i];
-	        }
-	        
-	        int maxIndex = 0;
-	        ArrayList<Integer> list = new ArrayList<>();
-	        for(int i=0 ; i<8001 ; i++){
-	            if(check[maxIndex] < check[i]){
-	                maxIndex = i;
-	                list.clear();
-	            }
-	            else if(check[i]!=0 && check[i]==check[maxIndex]){
-	                list.add(i-4000);
-	            }
-	        }
-	        
-	        Arrays.sort(arr);
-	        
-	        System.out.println((int)Math.round((double)sum/N));
-	        System.out.println(arr[N/2]);
-	        if(list.size()!=0)
-	            System.out.println(list.get(0));
-	        else
-	            System.out.println(maxIndex-4000);
-	        System.out.println(arr[N-1]-arr[0]);
-	    }
+	
 }
